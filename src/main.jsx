aa4903b285feebc2cec4673d22906c393892ff46
RezaualHossenSunny <sunnyrahman542@gmail.com>
2024-09-25 20:27:18 +0600

    router setup done

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Singin from './component/Singin.jsx';
+import Singup from './component/Singup.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/singin",
+    element: <Singin/>,
+  },
+  {
+    path: "/singnup",
+    element: <Singup/>,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  <RouterProvider router={router} />
   </StrictMode>,
 )
